9dfe6df4c34af86b35bc184a563c188d348b2b5f
everett-wxy <74697318+everett-wxy@users.noreply.github.com>
2024-11-08 01:18:55 +0800

    programmed routers

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -14,17 +14,46 @@ const getAllQuotes = async (req, res) => {
 
 const postQuotes = async (req, res) => {
     try {
-        const newQuotes = await Quote.create({
-            author: req.body.author,
-            quote: req.body.quote,
-            year: req.body.year,
-        });
+        // const newQuotes = await Quote.create({
+        //     author: req.body.author,
+        //     quote: req.body.quote,
+        //     year: req.body.year,
+        // });
+        const newQuotes = await Quote.create(req.body);
         console.log("New quote create: ", newQuotes);
         res.status(201).json(newQuotes); // Send the created quote back with a 201 status
     } catch (error) {
-        console.log("Error creating new quotes: ", error);
+        console.error("Error creating new quotes: ", error);
         throw error;
     }
 };
 
-module.exports = { getAllQuotes, postQuotes };
+const deleteQuotes = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteQuote = await Quote.findByIdAndDelete(id);
+        if (!deleteQuote) {
+            return res.status(400).json({ message: "Quote not found" });
+        }
+        res.status(200).json({ message: "Quote deleted successfully" });
+    } catch (error) {
+        console.error("Error creating new quotes: ", error);
+        throw error;
+    }
+};
+
+const updateQuotes = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuote = await Quote.findByIdAndUpdate(id, req.body);
+        if (!updateQuote) {
+            return res.status(400).json({ message: "Quote not found" });
+        }
+        const updatedQuote = await Quote.findById(id);
+        res.status(200).json(updatedQuote);
+    } catch (error) {
+        console.error("Error update quotes ", error);
+    }
+};
+
+module.exports = { getAllQuotes, postQuotes, deleteQuotes, updateQuotes };
--- src/routers/quoteRouter.js
+++ src/routers/quoteRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getAllQuotes, postQuotes } = require("../controllers/quoteController");
+const { getAllQuotes, postQuotes, deleteQuotes, updateQuotes } = require("../controllers/quoteController");
 const router = express.Router();
 
 router.get("/", getAllQuotes);
 router.post("/create", postQuotes);
+router.delete('/delete/:id',deleteQuotes)
+router.put('/user/:id', updateQuotes)
 
 module.exports = router;
